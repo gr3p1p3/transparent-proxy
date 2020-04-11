963ff10b63e781f84f451bed93a1fecc903ded0e
Gr3p <eddy.dejana1991@gmail.com>
2020-04-11 10:39:19 -0800

    init project

---
+++ ProxyServer.js
@@ -0,0 +1,150 @@
+const CLRF = '\r\n';
+const net = require('net');
+
+function socketWrite(tunnel, data) {
+    if (tunnel && tunnel.socket && !tunnel.socket.destroyed) {
+        tunnel.socket.write(data);
+    }
+}
+
+function clientWrite(tunnel, data) {
+    if (tunnel && tunnel.client && !tunnel.client.destroyed) {
+        tunnel.client.write(data);
+    }
+}
+
+function isUsingUpstreamToProxy(upstream, {data, bridgedConnection, remoteID}) {
+    return ((upstream && typeof upstream === 'function')
+        && (upstream(data, bridgedConnection, remoteID) !== 'localhost'))
+}
+
+function ProxyServer(options) {
+    let {upstream, injectData} = options || {};
+    const bridgedConnections = {};
+
+    function resetSockets(tunnel, id) {
+        if (tunnel) {
+            if (tunnel.client && !tunnel.client.destroyed) {
+                tunnel.client.destroy();
+            }
+            if (tunnel.socket && !tunnel.socket.destroyed) {
+                tunnel.socket.destroy();
+            }
+            delete bridgedConnections[id];
+        }
+    }
+
+    const server = net.createServer(function onConnectedClient(clientSocket) {
+        const rport = clientSocket.remotePort;
+        const raddr = clientSocket.remoteAddress;
+        const remoteID = raddr + ':' + rport;
+        bridgedConnections[remoteID] = {};
+
+        bridgedConnections[remoteID].socket = clientSocket
+            .on('data', function (data) {
+                const dataString = data.toString();
+                if (dataString && dataString.length) {
+                    const split = dataString.split(CLRF);
+                    if (~split[0].indexOf('CONNECT')) { //managing HTTP-Tunnel & HTTPs
+                        let ADDRESS, PORT;
+                        const upstreamHost = split[0].split(' ')[1];
+                        if (isUsingUpstreamToProxy(upstream, {
+                            data,
+                            bridgedConnection: bridgedConnections[remoteID],
+                            remoteID
+                        })) {
+                            const proxyInUse = upstream(data, bridgedConnections[remoteID], remoteID);
+                            ADDRESS = proxyInUse.split(':')[0].replace(/\//gmi, '');
+                            PORT = proxyInUse.split(':')[1];
+                        } else {
+                            ADDRESS = upstreamHost.split(':')[0].replace(/\//gmi, '');
+                            PORT = upstreamHost.split(':')[1];
+                        }
+                        bridgedConnections[remoteID].tunnel = {ADDRESS, PORT};
+                        bridgedConnections[remoteID].client = new net.Socket();
+
+                        bridgedConnections[remoteID].client
+                            .connect(PORT, ADDRESS, function onTunnelConnectionOpen() {
+                                if (isUsingUpstreamToProxy(upstream, {
+                                    data,
+                                    bridgedConnection: bridgedConnections[remoteID],
+                                    remoteID
+                                })) { //TODO HERE the problem with upstream... if upstream dont return proxy but direct host:port...it will
+                                    data = (injectData && typeof injectData === 'function') ? injectData(data, bridgedConnections[remoteID], remoteID) : data;
+                                    clientWrite(bridgedConnections[remoteID], data)
+                                } else {
+                                    socketWrite(bridgedConnections[remoteID], 'HTTP/1.0 200 OK' + CLRF + CLRF);
+                                }
+                            })
+                            .on('data', function (dataFromUpStream) {
+                                socketWrite(bridgedConnections[remoteID], dataFromUpStream);
+                            })
+                            .on('close', function () {
+                                resetSockets(bridgedConnections[remoteID], remoteID);
+                            })
+                            .on('error', function handleError(err) {
+                                console.log('ERROR=>', err.code, remoteID);
+                                resetSockets(bridgedConnections[remoteID], remoteID);
+                            });
+
+                    } else if (split[0].indexOf('CONNECT') === -1 && !bridgedConnections[remoteID].client) { // managing http
+                        let ADDRESS, PORT;
+                        const upstreamHost = split[0].split(' ')[1];
+                        if (isUsingUpstreamToProxy(upstream, {
+                            data,
+                            bridgedConnection: bridgedConnections[remoteID],
+                            remoteID
+                        })) {
+                            const proxyToUse = upstream(data, bridgedConnections[remoteID], remoteID);
+                            ADDRESS = proxyToUse.split(':')[0].replace(/\//gmi, '');
+                            PORT = proxyToUse.split(':')[1];
+                        } else {
+                            ADDRESS = upstreamHost.split(':')[1].replace(/\//gmi, '');
+                            PORT = upstreamHost.split(':')[2] || (~upstreamHost.split(':')[0].indexOf('https')) ? 443 : 80;
+                        }
+
+                        bridgedConnections[remoteID].tunnel = {ADDRESS, PORT};
+                        bridgedConnections[remoteID].client = new net.Socket();
+
+                        bridgedConnections[remoteID].client
+                            .connect(PORT, ADDRESS, function onDirectConnectionOpen() {
+                                data = (injectData && typeof injectData === 'function') ? injectData(data, bridgedConnections[remoteID], remoteID) : data;
+                                clientWrite(bridgedConnections[remoteID], data);
+                            })
+                            .on('data', function (dataFromUpStream) {
+                                socketWrite(bridgedConnections[remoteID], dataFromUpStream)
+                            })
+                            .on('close', function () {
+                                resetSockets(bridgedConnections[remoteID], remoteID);
+                            })
+                            .on('error', function handleError(err) {
+                                console.log('ERROR=>', err.code, remoteID);
+                                resetSockets(bridgedConnections[remoteID], remoteID);
+                            });
+
+                    } else if (bridgedConnections[remoteID] && bridgedConnections[remoteID].client) {
+                        data = (injectData && typeof injectData === 'function') ? injectData(data, bridgedConnections[remoteID], remoteID) : data;
+                        clientWrite(bridgedConnections[remoteID], data);
+                    }
+                }
+            })
+            .on('error', function handleError(err) {
+                console.log('ERROR=>', err.code, remoteID);
+                resetSockets(bridgedConnections[remoteID], remoteID);
+            })
+            .on('close', function () {
+                resetSockets(bridgedConnections[remoteID], remoteID);
+            })
+            .on('exit', function () {
+                resetSockets(bridgedConnections[remoteID], remoteID);
+            });
+    });
+
+    server.getBridgedConnections = function () {
+        return bridgedConnections;
+    };
+
+    return server;
+}
+
+module.exports = ProxyServer;
---
+++ README.md
@@ -0,0 +1,89 @@
+# Intro
+
+transparent-proxy is a http-proxy that acts in a transparent way.
+
+# Quick Start
+
+## Install
+
+```bash
+npm i transparent-proxy
+```
+
+```bash
+npm install -g transparent-proxy
+```
+
+## Use
+
+```javascript
+const ProxyServer = require('transparent-proxy');
+
+//init ProxyServer
+const server = ProxyServer();
+
+//starting server on port 8080
+server.listen(8080, '0.0.0.0', function () {
+    console.log('TCP-Proxy-Server started!', server.address());
+});
+```
+
+## Options Object
+
+| Param  | Type                | Description  |
+| ------ | ------------------- | ------------ |
+|options | <code>Object</code> |  The options object. |
+|[options.upstream] | <code>Function</code> |  The proxy to be used to upstreaming requests. |
+
+
+## Upstream to other proxy
+
+If you don't want to use the host of active instance self, then you need to upstream connections to another http-proxy.
+This can be done with `upstream` attribute.
+
+```javascript
+const ProxyServer = require('transparent-proxy');
+
+const server = ProxyServer({
+    upstream: function () {
+          return 'x.x.x.x:3128'; // upstream to other proxy
+    }
+});
+
+//starting server on port 8080
+server.listen(8080, '0.0.0.0', function () {
+    console.log('TCP-Proxy-Server started!', server.address());
+});
+```
+
+## Upstream
+
+| Param  | Type                | Description  |
+| ------ | ------------------- | ------------ |
+|data | <code>Buffer</code> |  The received data. |
+|bridgedConnection | <code>Socket</code> |  The socket instance |
+|bridgedConnectionId | <code>String</code> |  The id of connection. (IP:PORT) |
+
+## Examples
+
+This example upstreams only requests to ifconfig.me to another proxy, for all other requests will be used localhost.
+
+```javascript
+const ProxyServer = require('transparent-proxy');
+
+const server = ProxyServer({
+    upstream: function (data, bridgedConnection, bridgedConnectionId) {
+        if (~(data.toString().indexOf('ifconfig.me'))) {
+            return 'x.x.x.x:3128'; // upstream to other proxy
+        } else {
+            return 'localhost' //upstream to localhost
+        }
+    },
+});
+
+//starting server on port 8080
+server.listen(8080, '0.0.0.0', function () {
+    console.log('TCP-Proxy-Server started!', server.address());
+});
+```
+
