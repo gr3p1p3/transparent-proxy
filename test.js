39975e30d42cac3d1aa10b8786e414c38e91b88e
Gr3p <eddy.dejana1991@gmail.com>
2020-04-11 11:06:01 -0800

    add test for npm

---
+++ test.js
@@ -0,0 +1,14 @@
+const ProxyServer = require('./ProxyServer');
+
+//init ProxyServer
+const server = ProxyServer();
+
+//starting server on port 1555
+server.listen(8080, '0.0.0.0', function () {
+    console.log('transparent-proxy Server was started!', server.address());
+});
+
+setTimeout(function closeProxyServer() {
+    console.log('closing transparent-proxy Server');
+    server.close();
+}, 5000);
